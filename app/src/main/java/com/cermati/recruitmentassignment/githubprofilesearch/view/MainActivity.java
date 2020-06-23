3b057b575f8f71f36723a31cf4ae145ff33891f1
Izzan Fakhril Islam <izzanfi@hotmail.com>
2020-06-23 17:16:35 +0700

    add files

---
+++ app/src/main/java/com/cermati/recruitmentassignment/githubprofilesearch/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cermati.recruitmentassignment.githubprofilesearch.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cermati.recruitmentassignment.githubprofilesearch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
