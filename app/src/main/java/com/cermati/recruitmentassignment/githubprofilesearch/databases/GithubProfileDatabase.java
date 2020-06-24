250389f3e7150df75ff175b4630c3a013f48d80c
Izzan Fakhril Islam <izzanfi@hotmail.com>
2020-06-24 12:40:49 +0700

    Milestone 1: Setup App UI (#1)
    * add profile detail activity
    
    * add username card
    
    * add username card, and placeholder user picture
    
    * add mvvm basics

---
+++ app/src/main/java/com/cermati/recruitmentassignment/githubprofilesearch/databases/GithubProfileDatabase.java
@@ -0,0 +1,30 @@
+package com.cermati.recruitmentassignment.githubprofilesearch.databases;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.cermati.recruitmentassignment.githubprofilesearch.dao.GithubProfileDao;
+import com.cermati.recruitmentassignment.githubprofilesearch.model.GithubProfile;
+
+@Database(entities = {GithubProfile.class}, version = 1, exportSchema = false)
+public abstract class GithubProfileDatabase extends RoomDatabase{
+
+    private static GithubProfileDatabase instance;
+
+    public abstract GithubProfileDao githubProfileDao();
+
+    public static synchronized GithubProfileDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    GithubProfileDatabase.class,
+                    "github_profile_database"
+            ).fallbackToDestructiveMigration().build();
+        }
+
+        return instance;
+    }
+}
